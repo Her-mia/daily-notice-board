564e046a35b30129d445c392edbcba4b1e2538c1
hermia <hermia.wu@outlook.com>
2026-02-02 19:48:16 +0800

    update flask

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>我的看板环境搭建成功！</h1>"
+    data={"weather":"weather",
+          "countdown":"coutdown",
+          "timer":"tomato"
+    }
+    return render_template("index.html", data=data)
 
 if __name__ == '__main__':
     # debug=True 允许你修改代码保存后，网页自动刷新
