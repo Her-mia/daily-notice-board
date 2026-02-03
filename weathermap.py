697467fa2afa4c8613b507a2361aac156691d02e
hermia <hermia.wu@outlook.com>
2026-02-03 22:21:33 +0800

    add weather map

---
+++ weathermap.py
@@ -0,0 +1,29 @@
+weather_map = {
+    0:  ("晴", "sunny"),
+    1:  ("多云", "partly_cloudy"),
+    2:  ("局部多云", "cloudy"),
+    3:  ("阴", "overcast"),
+
+    45: ("有雾", "fog"),
+    48: ("霜雾", "fog"),
+
+    51: ("小毛毛雨", "drizzle"),
+    53: ("中毛毛雨", "drizzle"),
+    55: ("大毛毛雨", "drizzle"),
+
+    61: ("小雨", "rain"),
+    63: ("中雨", "rain"),
+    65: ("大雨", "rain"),
+
+    71: ("小雪", "snow"),
+    73: ("中雪", "snow"),
+    75: ("大雪", "snow"),
+
+    80: ("小阵雨", "rain_shower"),
+    81: ("中阵雨", "rain_shower"),
+    82: ("强阵雨", "rain_shower"),
+
+    95: ("雷暴", "thunder"),
+    96: ("雷暴伴轻微冰雹", "thunder_hail"),
+    99: ("雷暴伴强冰雹", "thunder_hail"),
+}
